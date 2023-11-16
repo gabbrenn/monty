72583f0537481ae6fb9c3eed8b2ec2bfd2288fc2
gabbrenn <gabvladimirbrenn@gmail.com>
2023-11-15 23:43:55 -0800

    function

--- function_1.c
+++ function_1.c
@@ -1,93 +1,83 @@
-/* stack_operations.c */
 #include "monty.h"
-
-stack_t *head = NULL;
-
 /**
- * add_to_stack - Adds a new node at the beginning of the stack.
- * @n: The integer value to be stored in the new node.
- * Return: A pointer to the newly added node.
+ * add_to_stack - creates a new node at the beginning of the list
+ * @head: first node
+ * @n: number to add
+ * Return: new added node
  */
+stack_t *head = NULL;
 stack_t *add_to_stack(const int n)
 {
-    stack_t *new;
+	stack_t *new;
 
-    new = malloc(sizeof(stack_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->n = n;
-    new->next = head;
-    new->prev = NULL;
-
-    if (head != NULL)
-        head->prev = new;
-
-    head = new;
-    return (new);
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = head;
+	new->prev = NULL;
+	if (head != NULL)
+		(head)->prev = new;
+	head = new;
+	return (new);
 }
-
 /**
- * print_all - Prints all elements in the stack.
- * Return: The number of printed nodes.
+ * print_all - prints all elements in the list
+ * @h: first node
+ * Return: number of printed nodes
  */
 size_t print_all(void)
 {
-    size_t printed = 0;
-    stack_t *current = head;
+	int printed;
 
-    while (current)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        printed++;
-    }
-    return (printed);
+	for (printed = 0; head; printed++)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+	}
+	return (printed);
 }
-
 /**
- * _strcmp - Compares two strings.
- * @s1: The first string to compare.
- * @s2: The second string to compare.
- * Return: 0 if equal, non-zero otherwise.
+ *_strcmp - will compare two strings
+ *@s1: first string to compare
+ *@s2: second string to check
+ *Return: 1 if true else 0;
  */
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
-    {
-        s1++;
-        s2++;
-    }
+	int ret;
 
-    return (*s1 - *s2);
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		ret = 0;
+	else
+		ret = *s1 - *s2;
+	return (ret);
 }
-
 /**
- * _realloc - Resizes the size of a pre-allocated pointer.
- * @ptr: The address to resize.
- * @size: The new size to allocate.
- * Return: A pointer to the newly allocated memory.
+ * _realloc - resizes the size of pre allocated pointer
+ * @ptr: address to resize
+ * @size: size to put the ptr to
+ * Return: pointer t newly acated memory
  */
 void *_realloc(void *ptr, size_t size)
 {
-    size_t i;
-    char *cpy, *temp;
+	size_t new_s;
+	void *cpy;
 
-    if (size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
-
-    cpy = malloc(size);
-    if (cpy == NULL)
-        return (NULL);
-
-    temp = ptr;
-    for (i = 0; i < size; i++)
-        cpy[i] = temp[i];
-
-    free(ptr);
-    return (cpy);
+	if (size == 0)
+		free(ptr);
+	else
+	{
+		cpy = malloc(sizeof(char) * size);
+		if (size > malloc_usable_size(ptr))
+			new_s = malloc_usable_size(ptr);
+		memcpy(cpy, ptr, new_s);
+		free(ptr);
+	}
+	return (cpy);
 }
-
