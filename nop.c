84cb8fc16dff79ae057df14527ed0be58cf51525
gabbrenn <gabvladimirbrenn@gmail.com>
2023-11-18 22:37:38 -0800

    monty

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing
+ * @head: head of stack
+ * @counter: number of lines
+ * Return: non return
+*/
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
