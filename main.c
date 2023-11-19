5572897f8ae5801fae5d62104ca8e742e83203dd
gabbrenn <gabvladimirbrenn@gmail.com>
2023-11-18 21:16:53 -0800

    Main function

--- main.c
+++ main.c
@@ -1,24 +1,45 @@
-#include"monty.h"
-int main(int argc, char **argv){
+#include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
+/**
+* main - Thi is Monty Code Main function
+* @argc: Number of arguments
+* @argv: location of monty file
+* Return: will be 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	char *content;
 	FILE *file;
-	char buf[1024];
-	
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
-	file = fopen(argv[1],"r");
-	if(!file)
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
 	{
-	   fprintf(stderr,"Error:Can't open file %s\n", argv[1]);
-	   exit(EXIT_FAILURE);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
-
-	while(fgets(buf,1024,file))
+	while (read_line > 0)
 	{
-	    printf("%s\n",buf);
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
 	}
-
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
